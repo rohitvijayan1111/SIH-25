3acbbf193784ebb250a846bc9fba4b2c661ac4b5
Shriram761 <shriramvenkatesh76@gmail.com>
2025-07-02 22:18:21 +0530

    Fixed Styling global.css

--- frontend/app/App.js
+++ frontend/app/App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
-
+import "./global.css";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center">
