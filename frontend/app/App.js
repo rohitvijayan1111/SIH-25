da2825546cb12c56b6150fc96279692b20c45106
Shriram761 <shriramvenkatesh76@gmail.com>
2025-07-02 21:39:22 +0530

    Frontend Setup and Tailwind Setup

---
+++ frontend/app/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl font-bold text-blue-700">Welcome</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
