be235fcfb5512e509b8a7cd1b832664ac6697111
Shriram <shriramvenkatesh76@gmail.com>
2025-07-06 16:13:22 +0530

    Delete frontend directory

--- frontend/app/index.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { Redirect } from "expo-router";
-
-const index = () => {
-  return <Redirect href="/home" />;
-};
-
-export default index;
