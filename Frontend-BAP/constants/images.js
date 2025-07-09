3699a792ad5c109073b460fa2fadc275e2c543d2
Shriram761 <shriramvenkatesh76@gmail.com>
2025-07-09 22:07:57 +0530

    Changed Styling for categories section ,header

---
+++ Frontend-BAP/constants/images.js
@@ -0,0 +1,18 @@
+import fertilizer from "../assets/images/categories/fertilizer.png";
+import fungicide from "../assets/images/categories/fungicide.png";
+import growthpromoter from "../assets/images/categories/growthpromoter.png";
+import growthregulator from "../assets/images/categories/growthregulator.png";
+import herbicide from "../assets/images/categories/herbicide.png";
+import landlease from "../assets/images/categories/landlease.png";
+import micronutrients from "../assets/images/categories/micronutrients.png";
+import seeds from "../assets/images/categories/seeds.png";
+export const images = {
+  fertilizer,
+  fungicide,
+  growthpromoter,
+  growthregulator,
+  herbicide,
+  landlease,
+  micronutrients,
+  seeds,
+};
--- Frontend-BAP/screens/HomePage.js
+++ Frontend-BAP/screens/HomePage.js
@@ -1,34 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
   FlatList,
   ActivityIndicator,
   ScrollView,
-  TextInput
-} from 'react-native';
-import { TouchableOpacity, Image } from 'react-native';
-import tw from 'tailwind-react-native-classnames';
-import CategorySection from '../components/CategorySection';
+  TextInput,
+} from "react-native";
+import { TouchableOpacity, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import CategorySection from "../components/CategorySection";
+import { LinearGradient } from "expo-linear-gradient";
+import { images } from "../constants/images";
 
+// const categoryList = [
+//   { id: "seed", name: "Seeds" },
+//   { id: "micro", name: "Micro Nutrient" },
+//   { id: "fertilizer", name: "Fertilizer" },
+//   { id: "fungicide", name: "Fungicide" },
+//   { id: "growth-promoter", name: "Growth Promoter" },
+//   { id: "growth-regulators", name: "Growth Regulators" },
+//   { id: "herbicide", name: "Herbicide" },
+//   { id: "land", name: "Land Lease & Sale" },
+// ];
 
-
-const categoryList = [
-  { id: 'seed', name: 'Seeds' },
-  { id: 'micro', name: 'Micro Nutrient' },
-  { id: 'fertilizer', name: 'Fertilizer' },
-  { id: 'fungicide', name: 'Fungicide' },
-  { id: 'growth-promoter', name: 'Growth Promoter' },
-  { id: 'growth-regulators', name: 'Growth Regulators' },
-  { id: 'herbicide', name: 'Herbicide' },
-  { id: 'land', name: 'Land Lease & Sale' },
+const categoriesforgrid = [
+  { id: "seed", name: "Seeds", icon: images.seeds },
+  { id: "micro", name: "Micro Nutrients", icon: images.micronutrients },
+  { id: "fertilizer", name: "Fertilizer", icon: images.fertilizer },
+  { id: "fungicide", name: "Fungicide", icon: images.fungicide },
+  {
+    id: "growth-promoter",
+    name: "Growth Promoter",
+    icon: images.growthpromoter,
+  },
+  {
+    id: "growth-regulators",
+    name: "Growth Regulators",
+    icon: images.growthregulator,
+  },
+  { id: "herbicide", name: "Herbicide", icon: images.herbicide },
+  { id: "land", name: "Land Lease & Sale", icon: images.landlease },
 ];
 
-
-const HomePage = ({navigation}) => {
+const HomePage = ({ navigation }) => {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     loadInitialCategories();
@@ -36,23 +54,23 @@ const HomePage = ({navigation}) => {
 
   const loadInitialCategories = async () => {
     setLoading(true);
-    Promise.all([fetchCategoryProducts('crop'), fetchCategoryProducts('dairy')])
+    Promise.all([fetchCategoryProducts("crop"), fetchCategoryProducts("dairy")])
       .then((results) => setCategories(results))
       .finally(() => setLoading(false));
   };
 
   const fetchCategoryProducts = async (category) => {
     try {
-      const response = await fetch('http://localhost:5000/bap/search', {
-        method: 'POST',
+      const response = await fetch("http://localhost:5000/bap/search", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          productName: '',
+          productName: "",
           category: category,
-          lat: '13.0827',
-          lon: '80.2707',
+          lat: "13.0827",
+          lon: "80.2707",
           radius: 300,
         }),
       });
@@ -84,16 +102,16 @@ const HomePage = ({navigation}) => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://192.168.199.249:5000/bap/search', {
-        method: 'POST',
+      const response = await fetch("http://192.168.199.249:5000/bap/search", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           productName: name,
-          category: '',
-          lat: '13.0827',
-          lon: '80.2707',
+          category: "",
+          lat: "13.0827",
+          lon: "80.2707",
           radius: 300,
         }),
       });
@@ -109,47 +127,46 @@ const HomePage = ({navigation}) => {
         },
       ]);
     } catch (error) {
-      console.error('Error searching product name:', error);
+      console.error("Error searching product name:", error);
     } finally {
       setLoading(false);
     }
   };
-  
-  const handleCategoryPress = async (categoryID,categoryName) => {
-  setLoading(true);
 
-  try {
-    const response = await fetch('http://localhost:5000/bap/search', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        productName: '',
-        category: categoryID.toLowerCase(),
-        lat: '13.0827',
-        lon: '80.2707',
-        radius: 300,
-      }),
-    });
+  const handleCategoryPress = async (categoryID, categoryName) => {
+    setLoading(true);
 
-    const json = await response.json();
-    const catalog = json.catalog?.message?.catalog;
+    try {
+      const response = await fetch("http://localhost:5000/bap/search", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          productName: "",
+          category: categoryID.toLowerCase(),
+          lat: "13.0827",
+          lon: "80.2707",
+          radius: 300,
+        }),
+      });
 
-    setCategories([
-      {
-        category: categoryName,
-        items: catalog?.items || [],
-        providers: catalog?.providers || [],
-      },
-    ]);
-  } catch (error) {
-    console.error('Category fetch error:', error);
-  } finally {
-    setLoading(false);
-  }
-};
-  
+      const json = await response.json();
+      const catalog = json.catalog?.message?.catalog;
+
+      setCategories([
+        {
+          category: categoryName,
+          items: catalog?.items || [],
+          providers: catalog?.providers || [],
+        },
+      ]);
+    } catch (error) {
+      console.error("Category fetch error:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   if (loading) {
     return (
@@ -161,9 +178,24 @@ const HomePage = ({navigation}) => {
   }
 
   return (
-    <View style={tw`flex-1 bg-gray-50`}>
-      {/* Search Bar */}
-      <View style={tw`px-4 pt-4 pb-2 bg-white shadow-sm`}>
+    <View style={tw`flex-1`}>
+      <LinearGradient
+        colors={["#B2FFB7", "#ffffff"]}
+        style={tw`px-2 pt-4 pb-4 rounded-b-4xl shadow-md`}
+      >
+        <View style={tw`flex-row justify-between items-center mb-2`}>
+          <Text style={tw`text-lg font-extrabold text-green-800`}>
+            FarmNFresh
+          </Text>
+          <View style={tw`flex-row items-center space-x-2`}>
+            <View style={tw`bg-white px-2 py-1 rounded-md`}>
+              <Text style={tw`text-[#2B9846] font-semibold text-sm`}>
+                ₹20000
+              </Text>
+            </View>
+          </View>
+        </View>
+
         <View style={tw`flex-row items-center`}>
           <TextInput
             placeholder="Search products..."
@@ -179,29 +211,71 @@ const HomePage = ({navigation}) => {
             <Text style={tw`text-white font-medium`}>Search</Text>
           </TouchableOpacity>
         </View>
+      </LinearGradient>
+      {/*Ajith Search Bar*/}
+      {/* <View style={tw`px-4 pt-4 pb-2 bg-white shadow-sm`}>
+        <View style={tw`flex-row items-center`}>
+          <TextInput
+            placeholder="Search products..."
+            placeholderTextColor="#9CA3AF"
+            value={searchTerm}
+            onChangeText={setSearchTerm}
+            style={tw`flex-1 bg-gray-100 rounded-lg px-4 py-2 text-gray-800`}
+          />
+          <TouchableOpacity
+            onPress={() => searchProductsByName(searchTerm)}
+            style={tw`ml-2 bg-blue-500 px-4 py-2 rounded-lg shadow`}
+          >
+            <Text style={tw`text-white font-medium`}>Search</Text>
+          </TouchableOpacity>
+        </View>
+      </View> */}
+      {/* My Category List */}
+      <View style={tw`mt-2`}>
+        <FlatList
+          data={categoriesforgrid}
+          numColumns={4}
+          // showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => handleCategoryPress(item.id, item.name)}
+              style={tw`flex-1 justify-center items-center p-3 m-1 bg-white rounded-xl shadow-md`}
+            >
+              <Image
+                source={item.icon}
+                className="w-8 h-8"
+                resizeMode="contain"
+              />
+              <Text numberOfLines={1} className="text-center text-xs mt-2">
+                {item.name}
+              </Text>
+            </TouchableOpacity>
+          )}
+          contentContainerStyle={tw`px-2`}
+        />
       </View>
-        
-        {/*Category */}
-        <View style={tw`mt-2`}>
-  <FlatList
-    data={categoryList}
-    horizontal
-    showsHorizontalScrollIndicator={false}
-    keyExtractor={(item) => item.id}
-    renderItem={({ item }) => (
-      <TouchableOpacity
-        onPress={() => handleCategoryPress(item.id,item.name)}
-        style={tw`bg-green-200 px-4 py-2 rounded-full mx-1`}
-      >
-        <Text style={tw`text-green-800 font-medium text-sm`}>
-          {item.name}
-        </Text>
-      </TouchableOpacity>
-    )}
-    contentContainerStyle={tw`px-2`}
-  />
-</View>
 
+      {/* Ajith Category List
+      <View style={tw`mt-2`}>
+        <FlatList
+          data={categoryList}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => handleCategoryPress(item.id, item.name)}
+              style={tw`bg-green-200 px-4 py-2 rounded-full mx-1`}
+            >
+              <Text style={tw`text-green-800 font-medium text-sm`}>
+                {item.name}
+              </Text>
+            </TouchableOpacity>
+          )}
+          contentContainerStyle={tw`px-2`}
+        />
+      </View> */}
 
       {/* Content */}
       <ScrollView contentContainerStyle={tw`pb-8`}>
@@ -221,7 +295,6 @@ const HomePage = ({navigation}) => {
       </ScrollView>
     </View>
   );
-
 };
 
 export default HomePage;
